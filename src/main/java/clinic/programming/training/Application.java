89bd1246249ae073baecb3c6ede311fa6835ec20
Łukasz Pieniek <lpieniek@pzu.pl>
2018-03-11 11:29:53 +0100

    Dodanie definicji w pom dla pluginu commpiler

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,26 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
 	public Application() {
 		System.out.println("Inside Application");
 	}
 
+	public void greet() {
+		List<String> lista= new ArrayList();
+		lista.add("Hello");
+		
+		for(String el: lista)
+			System.out.println(el);
+	}
+	
 	// method main(): ALWAYS the APPLICATION entry point
 	public static void main(String[] args) {
 		System.out.println("Starting Application");
 		Application app = new Application();
+		app.greet();
 	}
 }
